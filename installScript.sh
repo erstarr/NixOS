5385bc4eee2d40cd72cf20bfe0f514576431126a
erStarr <253168930+erstarr@users.noreply.github.com>
2026-07-14 16:51:24 +0200

    Update installScript.sh

--- installScript.sh
+++ installScript.sh
@@ -14,28 +14,30 @@ confirm() {
 # Failiure Mode
 set -euo pipefail
 
-FLAKE_PATH="${1:?Usage: install.sh <path-to-flake>#<hostname>}"
+FLAKE_DIR="${1:?Usage: install.sh <flake-dir> <disko-dir>}"
+DISKO_DIR="${2:?Usage: install.sh <flake-dir> <disko-dir>}"
 
-echo "Flake Path: $FLAKE_PATH"
-echo "Disko Config Path: $(dirname "$0")/disko.nix"
 
-confirm "STEP 1: Wipe, partition, format and mount disk. THIS IS DESTRUCTIVE. Continue?"
+echo "Flake Dir: $FLAKE_PATH"
+echo "Disko Dir:  $DISKO_DIR"
 
 # 1. Wipe, partition, format, mount
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount     "$(dirname "$0")/disko.nix"
+confirm "STEP 1: Wipe, partition, format and mount disk. THIS IS DESTRUCTIVE. Continue?"
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount     "$DISKO_DIR/disko.nix"
 
-
-confirm "STEP 2: Creating empty root-blank subvolume (read only). Continue?"
 
 # 2. Create an empty root vol for impermemence
+confirm "STEP 2: Creating empty root-blank subvolume (read only). Continue?"
 sudo btrfs subvolume snapshot -r /mnt/partition-root/root /mnt/partition-root/root-blank
 
+
+# 3. Nix Install
 confirm "STEP 3: Run nixos-install. Continue (It'll hang until you provide a password)?"
-sudo nixos-install --flake "$FLAKE_PATH"
+sudo nixos-install --flake "$FLAKE_DIR"
 
 
+# 4. User Password setting - Username: redstar
 confirm "STEP 4: Setting user password. Continue?"
-
 sudo nixos-enter --root /mnt -c 'passwd redstar'
 
 
