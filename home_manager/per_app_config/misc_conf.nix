804e162a3adee4d8db2d2be6bcc4554c0ee3ff33
erstarr <253168930+erstarr@users.noreply.github.com>
2026-07-21 06:11:40 +0200

    smt

--- home_manager/per_app_config/misc_conf.nix
+++ home_manager/per_app_config/misc_conf.nix
@@ -3,40 +3,40 @@
 
 let
   dotDir = "NixOS_Config/dotfiles";
-in {
+in
+{
 
   # Hyprland
   xdg.configFile."hypr".source =
-    config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/${dotDir}/hypr";
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/${dotDir}/hypr";
 
   # Waybar
   xdg.configFile."waybar".source =
-    config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/${dotDir}/waybar";
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/${dotDir}/waybar";
 
   # rofi
   xdg.configFile."rofi".source =
-    config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/${dotDir}/rofi";
-
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/${dotDir}/rofi";
 
   # SwayNC
   xdg.configFile."swaync".source =
-    config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/${dotDir}/swaync";
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/${dotDir}/swaync";
 
   # Satty
   xdg.configFile."satty".source =
-    config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/${dotDir}/satty";
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/${dotDir}/satty";
 
+  # Kitty
+  xdg.configFile."kitty".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/${dotDir}/kitty";
 
   # Yazi
   xdg.configFile."yazi".source =
-    config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/${dotDir}/yazi";
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/${dotDir}/yazi";
 
+  # Wireplumber
+  xdg.configFile."wireplumber".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/${dotDir}/wireplumber";
 
   # home.file.".somerc".source =
   #   config.lib.file.mkOutOfStoreSymlink
