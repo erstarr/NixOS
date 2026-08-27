e19df1338dbdee628fca057db5348b760f722e82
erstarr <253168930+erstarr@users.noreply.github.com>
2026-08-27 21:54:30 +0000

    weeeeeeee

--- scripts/installScript.sh
+++ scripts/installScript.sh
@@ -84,7 +84,7 @@ sudo cp -r "$FLAKE_DIR/." /mnt/persist/home/redstar/.config/NixOS_Config/
 confirm "Step 6: Remove Channels since i'm using flake for nixpkgs -- DO NOT DO THIS IF YOU ARE USING CHANNELS!" 
 confirm "Step 6.1: See if the directory has anything more than 2 symlinks named channel* :"
 sudo ls -la /mnt/nix/var/nix/profiles/per-user/root/
-confirm "Step 6.2: Removing with -rf /mnt/nix/var/nix/profiles/per-user/root/channels*"
+confirm "Step 6.2: Removing with -f /mnt/nix/var/nix/profiles/per-user/root/channels*"
 sudo rm -r /mnt/nix/var/nix/profiles/per-user/root/channels*
 confirm "Step 6.3: See if the operation was a success:"
 sudo ls -la /mnt/nix/var/nix/profiles/per-user/root/
